d6a7578932a2240f113187096f90177dfef32d44
Z-Shoushou <37326777+Z-Shoushou@users.noreply.github.com>
2018-05-08 15:45:06 +0800

    1
    1

--- Discount calculate/Discount calculate（Private version）.py
+++ Discount calculate/Discount calculate（Private version）.py
@@ -6,12 +6,18 @@ Created on Tue Apr 17 20:15:09 2018
 """
 
 Real_pay = float(input('Please input your Amount of real pay: '))
-Original_pay = float(input('Pleas input your original pay: '))
-
-Ratio = Real_pay / Original_pay
-
+Commodity_price = float(input('Pleas input your commodity price: '))
+Meals_fee = float(input('Please input your meal fee : '))
+Delivery_cost = float(input('Please input your delivery cost : '))
 Personal_input = input('Please input your personal consumptions(Comma separated): ').split(',')
 
+Number_people = float(len(Personal_input))
+Original_pay = Meals_fee * Number_people + Delivery_cost + Commodity_price
+Ratio = Real_pay / Original_pay
+
+
 for index in range(len(Personal_input)) :
-    per_pay = float(Real_pay) / float(Original_pay) * float(Personal_input[index])
+    per_delivery = float(Delivery_cost / Number_people)
+    Personal_pay = float(Personal_input[index]) + Meals_fee + per_delivery
+    per_pay = float(Real_pay) / float(Original_pay) * float(Personal_pay)
     print ('Your personal real pay is ' + str(per_pay) + '.')
